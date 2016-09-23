d2905261c9038af4513707a312c814af6f0394c4
laura <laura@DESKTOP-MTFFKIV>
2016-09-23 17:08:11 -0400

    Initial checkin of Laura's sync with GitHub

---
+++ src/GitHub/TestData.java
@@ -0,0 +1,14 @@
+package GitHub;
+
+public class TestData {
+	
+	public static String displayText(String x) {
+		// TODO Auto-generated method stub
+		String text;
+		text = "This is the Test Data";
+		return text;
+	}
+
+	
+
+}
---
+++ src/GitHub/TestMain.java
@@ -0,0 +1,10 @@
+package GitHub;
+import GitHub.TestData;
+public class TestMain {
+	public static void main(String[]args){
+		String x = "x";
+		String ans = TestData.displayText(x);
+		System.out.println(ans);
+	}
+
+}
